15e10ec1394034bafaaca328a1100aeb5f62b5cb
navinash47 <navinash47@gmail.com>
2022-09-13 16:22:18 +0530

    added animation for bird

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,17 @@ public class Player : MonoBehaviour
     private Vector3 direction;
     public float gravity=-9.8f;
     public float strength = 5f;
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite),0.15f,0.15f);
+    }
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space)|| Input.GetMouseButtonDown(0))
@@ -21,5 +32,16 @@ public class Player : MonoBehaviour
                 direction = Vector3.up * strength;
             }
         }
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
+    }
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+        if (spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+        spriteRenderer.sprite = sprites[spriteIndex];
     }
 }
