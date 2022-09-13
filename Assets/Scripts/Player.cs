e77a6168fb75f218321007948585fa567be8df7a
navinash47 <navinash47@gmail.com>
2022-09-13 15:40:20 +0530

    Added first few files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 direction;
+    public float gravity=-9.8f;
+    public float strength = 5f;
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)|| Input.GetMouseButtonDown(0))
+        {
+            direction = Vector3.up * strength;
+        }
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                direction = Vector3.up * strength;
+            }
+        }
+    }
+}
